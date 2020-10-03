ed4b147733297dfa6db65a9e96918d9bb1222bcd
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-10-03 13:55:57 +0200

    Close #8, Add model for chords

---
+++ SCAPI/SCAPI_Frontend/Models/ChordModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SCAPI_Frontend.Models
+{
+    public class ChordModel
+    {
+        public int Id { get; set; }
+        public string BaseNote { get; set; }
+        public int FretPosition { get; set; }
+        public int StartString { get; set; }
+        public string TriadType { get; set; }
+        public string ColorNote { get; set; }
+        public ChordDiagramModel ChordDiagram { get; set; }
+    }
+}
